47e9b88ce12120c7797c03a1486061d56f4672af
jujugoboom <jujugoboom@gmail.com>
2015-12-01 13:46:48 -0500

    debugging

--- email_webapp/wsgi.py
+++ email_webapp/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "email_webapp.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
