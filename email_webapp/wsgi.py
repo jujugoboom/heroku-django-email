e87e04bb9c146f0797943d6248a17dbf9df984ed
jujugoboom <jujugoboom@gmail.com>
2015-12-01 14:03:34 -0500

    Debugging

--- email_webapp/wsgi.py
+++ email_webapp/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for email_webapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
 import os
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "email_webapp.settings")
 
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
