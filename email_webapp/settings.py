7d790008e1878d27adc7f97fefef5b57bf901781
jujugoboom <jujugoboom@gmail.com>
2015-12-01 12:04:31 -0500

    Turn on debugging

--- email_webapp/settings.py
+++ email_webapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wfn!8(-!unsjy-i&&dvi(*p953k9_z!-s97!99-diol(a27#=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
