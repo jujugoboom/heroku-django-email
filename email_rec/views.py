fa5864f926811155fc1fcd37e60c53edca292bc0
jujugoboom <jujugoboom@gmail.com>
2015-12-01 14:21:20 -0500

    Debugging

--- email_rec/views.py
+++ email_rec/views.py
@@ -10,46 +10,45 @@ API_KEY = 'f5fe8fa67f3fdb15a4a5a7f3788c5acb'
 # Create your views here.
 
 def index(request):
+    return HttpResponse("OK")
+
+def recieve_email(request):
+    print('NEW MESSAGE')
     if request.method == 'POST':
         print('NEW POST... CREATING MESSAGE OBJECT')
         recieve_email(request)
-        return HttpResponse("OK")
-    else:
-        return HttpResponse("OK")
-
-def recieve_email(request):
-    print("CREATING MESSAGE")
-    email = Message()
-    print("MESSAGE CREATED")
-    email.sender = request.POST.get('sender')
-    print("ADDED SENDER")
-    email.recipient = request.POST.get('recipient')
-    print("ADDED RECIPIENT")
-    email.subject = request.POST.get('subject', '')
-    print("ADDED SUBJECT")
-    email.message = request.POST.get('body-plain', '')
-    print("ADDED MESSAGE")
-    attachments = ''
-    print("ADDING ATTACHMENTS")
-    if len(request.FILES.keys()) > 0:
-        print("CONNECTING TO S3")
-        conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,default_bucket='justin-email-attachments')
-        print("CONNECTED TO S3")
-        for key in request.FILES:
-            print("ADDING FILE TO S3")
-            f = open(request.FILES[key],'rb')
-            attachments += f.name + ', '
-            conn.upload(f.name,f,'my_bucket')
-    email.attachments = attachments
-    print("ADDED ATTACHMENTS")
-    email.timestamp = request.POST.get('timestamp')
-    print("ADDED TIME STAMP")
-    if verify(API_KEY, request.POST.get('token'), request.POST.get('timestamp'), request.POST.get('signature')):
-        print("VERIFIED")
-        email.save()
-        print("SAVED")
-    else:
-        print("NOT VERIFIED")
+        print("CREATING MESSAGE")
+        email = Message()
+        print("MESSAGE CREATED")
+        email.sender = request.POST.get('sender')
+        print("ADDED SENDER")
+        email.recipient = request.POST.get('recipient')
+        print("ADDED RECIPIENT")
+        email.subject = request.POST.get('subject', '')
+        print("ADDED SUBJECT")
+        email.message = request.POST.get('body-plain', '')
+        print("ADDED MESSAGE")
+        attachments = ''
+        print("ADDING ATTACHMENTS")
+        if len(request.FILES.keys()) > 0:
+            print("CONNECTING TO S3")
+            conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,default_bucket='justin-email-attachments')
+            print("CONNECTED TO S3")
+            for key in request.FILES:
+                print("ADDING FILE TO S3")
+                f = open(request.FILES[key],'rb')
+                attachments += f.name + ', '
+                conn.upload(f.name,f,'my_bucket')
+        email.attachments = attachments
+        print("ADDED ATTACHMENTS")
+        email.timestamp = request.POST.get('timestamp')
+        print("ADDED TIME STAMP")
+        if verify(API_KEY, request.POST.get('token'), request.POST.get('timestamp'), request.POST.get('signature')):
+            print("VERIFIED")
+            email.save()
+            print("SAVED")
+        else:
+            print("NOT VERIFIED")
     return HttpResponse('OK')
 
 
