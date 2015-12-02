635756c947be61c5a6cd9a13ae044e6b1c55371b
jujugoboom <jujugoboom@gmail.com>
2015-12-01 19:09:01 -0500

    Debugging

--- email_rec/views.py
+++ email_rec/views.py
@@ -32,7 +32,7 @@ def recieve_email(request):
         if verify(API_KEY, request.POST.get('token'), request.POST.get('timestamp'), request.POST.get('signature')):
             email.save()
     elif request.method == 'GET':
-        print(request.GET.keys())
+        print(request.META.keys())
     return HttpResponse('OK')
 
 
