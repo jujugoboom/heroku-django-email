693208282a6ea4636d1d5c2da40a1299ee392a84
jujugoboom <jujugoboom@gmail.com>
2015-12-02 11:07:50 -0500

    Debugging

--- email_rec/forms.py
+++ email_rec/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from email_rec.models import FileModel
 
 class FileForm(ModelForm):
+
     class Meta:
         model = FileModel
-        fields = '__all__'
+        fields = 'file'
