39cf33820cfb4d27703bd83d817a17e402f83c09
jujugoboom <jujugoboom@gmail.com>
2015-12-03 21:59:10 -0500

    Debugging

--- email_rec/views.py
+++ email_rec/views.py
@@ -9,9 +9,7 @@ import base64
 import time
 import os
 from .models import *
-from .forms import *
-from hashlib import sha1
-import urllib.parse
+import boto3
 
 API_KEY = os.environ.get('API_KEY')
 USERNAME = os.environ.get('USERNAME')
@@ -19,6 +17,9 @@ PASSWORD = os.environ.get('PASSWORD')
 PUSH_TOKEN = os.environ.get("PUSH_TOKEN")
 PUSH_USER = os.environ.get("PUSH_USER")
 USERPASS = base64.b64encode(str(USERNAME + ':' + PASSWORD).encode())
+aws_access_key_id = os.environ.get('S3_ACCESS_KEY')
+aws_secret_access_key = os.environ.get('S3_SECRET_KEY')
+S3_BUCKET = os.environ.get('AWS_BUCKET_NAME')
 
 def index(request):
     return HttpResponse("OK")
@@ -36,13 +37,12 @@ def recieve_email(request):
             attachments = ''
             files = []
             if len(request.FILES.keys()) > 0:
+                s3 = boto3.resource('s3')
                 for key in request.FILES:
                     file = request.FILES[key]
                     print("UPLOADING " + file.name)
                     attachments += file.name
-                    signed_request = sign_s3(file.name, file.content_type)
-                    response = requests.put(signed_request, data=file)
-                    print('SERVER RESPONSE: ' + json.dumps(response.content))
+                    s3.Bucket(S3_BUCKET).put_object(Key=file.name, Body=file)
             email.attachments = attachments
             email.save()
             notification = {'token' : PUSH_TOKEN, 'user' : PUSH_USER, 'title' : 'New Email', 'message' : 'New Email from ' + email.sender + ' "' + email.subject + '"'}
@@ -68,23 +68,3 @@ def verify(api_key, token, timestamp, signature):
                              key=api_key,
                              msg='{}{}'.format(timestamp, token).encode(),
                              digestmod=hashlib.sha256).hexdigest()
-
-def sign_s3(filename, filetype):
-    AWS_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
-    AWS_SECRET_KEY = os.environ.get('S3_SECRET_KEY')
-    S3_BUCKET = os.environ.get('AWS_BUCKET_NAME')
-
-    object_name = urllib.parse.quote_plus(filename)
-    mime_type = filetype
-
-    expires = int(time.time()+60*60*24)
-    amz_headers = "x-amz-acl:public-read"
-
-    string_to_sign = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
-
-    signature = base64.encodebytes(hmac.new(AWS_SECRET_KEY.encode(), string_to_sign.encode('utf8'), sha1).digest()).decode()
-    signature = urllib.parse.quote_plus(signature.strip())
-
-    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
-    signed_request = '%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature)
-    return signed_request
