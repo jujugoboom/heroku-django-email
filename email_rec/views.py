3c17db425eb840828d88a3e2b7b892e57e303d0e
jujugoboom <jujugoboom@gmail.com>
2015-12-23 22:24:52 -0500

    revert

--- email_rec/views.py
+++ email_rec/views.py
@@ -21,41 +21,31 @@ S3_BUCKET = os.environ.get('AWS_BUCKET_NAME')
 def index(request):
     return HttpResponse("OK")
 
-def handle_request(request):
+def recieve_email(request):
     if request.method == 'POST':
-        return recieve_message(request)
+        email = Message()
+        email.sender = request.POST.get('sender')
+        email.recipient = request.POST.get('recipient')
+        email.subject = request.POST.get('subject', '')
+        email.message = request.POST.get('body-plain', '')
+        email.timestamp = int(request.POST.get('timestamp'))
+        if verify(API_KEY.encode(), request.POST.get('token'), request.POST.get('timestamp'), request.POST.get('signature')):
+            attachments = ''
+            if len(request.FILES.keys()) > 0:
+                s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+                for key in request.FILES:
+                    file = request.FILES[key]
+                    filename = file.name
+                    temp = filename.split('.')
+                    filename = ''.join(temp[:-1]) + str(email.timestamp) + '.' + str(temp[-1])
+                    attachments += filename + ', '
+                    s3.Bucket(S3_BUCKET).put_object(Key=filename, Body=file)
+            email.attachments = attachments
+            email.save()
+            notification = {'token' : PUSH_TOKEN, 'user' : PUSH_USER, 'title' : 'New Email', 'message' : 'New Email from ' + email.sender + ' "' + email.subject + '"'}
+            requests.post("https://api.pushover.net/1/messages.json", data=notification)
     elif request.method == 'GET':
-        return retreive_message(request)
-    elif request.method == 'DELETE':
-        delete_message(request)
-    return HttpResponse('OK')
-
-def recieve_message(request):
-    email = Message()
-    email.sender = request.POST.get('sender')
-    email.recipient = request.POST.get('recipient')
-    email.subject = request.POST.get('subject', '')
-    email.message = request.POST.get('body-plain', '')
-    email.timestamp = int(request.POST.get('timestamp'))
-    if verify(API_KEY.encode(), request.POST.get('token'), request.POST.get('timestamp'), request.POST.get('signature')):
-        attachments = ''
-        if len(request.FILES.keys()) > 0:
-            s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-            for key in request.FILES:
-                file = request.FILES[key]
-                filename = file.name
-                temp = filename.split('.')
-                filename = ''.join(temp[:-1]) + str(email.timestamp) + '.' + str(temp[-1])
-                attachments += filename + ', '
-                s3.Bucket(S3_BUCKET).put_object(Key=filename, Body=file)
-        email.attachments = attachments
-        email.save()
-        notification = {'token' : PUSH_TOKEN, 'user' : PUSH_USER, 'title' : 'New Email', 'message' : 'New Email from ' + email.sender + ' "' + email.subject + '"'}
-        requests.post("https://api.pushover.net/1/messages.json", data=notification)
-        return HttpResponse('OK')
-
-def retreive_message(request):
-    if request.META['HTTP_AUTHORIZATION'] == 'Basic ' + USERPASS.decode():
+        if request.META['HTTP_AUTHORIZATION'] == 'Basic ' + USERPASS.decode():
             if list(Message.objects.all()) == []:
                 return HttpResponse('No Messages')
             lastmessage = Message.objects.order_by('-id')[0]
@@ -66,9 +56,8 @@ def retreive_message(request):
                 id = int(lastmessage.id) - int(request.META['HTTP_ID'])
                 messages = Message.objects.all().order_by('-id')[id-50:id]
             return JsonResponse(serializers.serialize('json', messages), safe=False)
-
-def delete_message(request):
-    if request.META['HTTP_AUTHORIZATION'] == 'Basic ' + USERPASS.decode():
+    elif request.method == 'DELETE':
+        if request.META['HTTP_AUTHORIZATION'] == 'Basic ' + USERPASS.decode():
             i = int(request.META['HTTP_ID'])
             email = Message.objects.all().order_by('-id')[i]
             trash = TrashEmail()
@@ -80,6 +69,8 @@ def delete_message(request):
             trash.attachments = email.attachments
             trash.save()
             email.delete()
+    return HttpResponse('OK')
+
 
 def verify(api_key, token, timestamp, signature):
     return signature == hmac.new(
